016e1bea88c529faa9456184dff6773ee0211fea
Jonn Alves <jonatas.alsantos@jsl.com.br>
2021-08-02 19:37:38 -0300

    finish implementation BroadcastManagerExt.kt

---
+++ core/src/main/java/com/quanticheart/core/extentions/system/BroadcastManagerExt.kt
@@ -0,0 +1,44 @@
+package com.quanticheart.core.extentions.system
+
+import android.app.Activity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Bundle
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+
+private var broadcastManager: LocalBroadcastManager? = null
+private var reloadReceiver: BroadcastReceiver? = null
+
+@Suppress("unused")
+fun Activity.destroyBroadcastManager() {
+    reloadReceiver?.let {
+        broadcastManager?.unregisterReceiver(it)
+        reloadReceiver = null
+    }
+}
+
+fun Activity.createBroadcastManager(vararg actions: String, callback: (action: String) -> Unit) {
+    val filter = IntentFilter().apply {
+        actions.forEach {
+            addAction(it)
+        }
+    }
+
+    reloadReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            intent.action?.let { callback(it) }
+        }
+    }
+
+    LocalBroadcastManager.getInstance(this).apply {
+        broadcastManager = this
+        reloadReceiver?.let { this.registerReceiver(it, filter) }
+    }
+}
+
+fun Activity.sendBroadcastAction(action: String, extras: Bundle? = null) {
+    val intent = Intent(action).apply { extras?.let { putExtras(it) } }
+    LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
+}
